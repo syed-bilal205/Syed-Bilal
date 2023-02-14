01b7b7ea263defc3bc44ad11b52c0fcd7c87dad4
bilal <syedb4115@gmail.com>
2023-02-15 03:27:20 +0500

    may be it will solve the problem

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 "use client" 
 import React from 'react'
 import {Lines} from 'react-preloaders'
-import {motion,useScroll} from 'framer-motion'
+import HorizontalScroll from './HorizontalScroll'
 import Navbar from './Navbar'
 import Header from './Header'
 import About from './About'
@@ -13,12 +13,11 @@ import Footer from './Footer'
 
 const page = () => {
   
-  const { scrollYProgress } = useScroll();
 
   return (
     <>
       <Lines color={'#20c863'} background="#2d2e2e"/>
-      <motion.div className='fixed top-0 left-0 right-0 h-[10px] origin-left bg-[#20c863] z-50' style={{ scaleX:  scrollYProgress }} />
+      <HorizontalScroll/>
       <Navbar/>
       <Header/>
       <About/>
